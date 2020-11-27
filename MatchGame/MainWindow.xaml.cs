3c53c012c0bb4aa9d37c04ec0cf2bca440d76b2f
pmbos <pmbos.personal@gmail.com>
2020-11-27 15:19:33 +0100

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmojis = new List<string>
+            {
+                "🎅", "🎅",
+                "🤴", "🤴",
+                "🧛‍", "🧛‍",
+                "🧙‍", "🧙",
+                "🎃", "🎃",
+                "🎄", "🎄",
+                "🎆", "🎆",
+                "💎", "💎",
+                "🎁", "🎁",
+                "🧶", "🧶",
+            };
+
+            if (mainGrid.Children.OfType<TextBlock>().Count() != animalEmojis.Count)
+                throw new ArgumentException("There is a mismatch between the number of UI slots and the number of Emojis!");
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmojis.Count);
+                string nextEmoji = animalEmojis[index];
+                textBlock.Text = nextEmoji;
+                animalEmojis.RemoveAt(index);
+                
+            }
+        }
+    }
+}
